7d08caeec6b4931acfbda8db1daa5cf94251ac62
Keith Salisbury <keithsalisbury@gmail.com>
2010-06-15 14:32:39 +0100

    initial import

---
+++ db/migrate/20100615125340_create_elements.rb
@@ -0,0 +1,18 @@
+class CreateElements < ActiveRecord::Migration
+  def self.up
+    create_table :elements do |t|
+      t.string :name
+      t.string :ancestry
+      t.integer :ancestry_depth, :default => 0
+      t.integer :position
+      t.string :type
+
+      t.timestamps
+    end
+    add_index :elements, :ancestry
+  end
+
+  def self.down
+    drop_table :elements
+  end
+end
