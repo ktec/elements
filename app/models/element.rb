35e0961e32867f9e3ec5758c3a43a2e7cae8d0e9
Keith Salisbury <keithsalisbury@gmail.com>
2010-06-21 00:53:24 +0100

    set up element model for polymorphic attachements

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,2 +1,8 @@
 class Element < ActiveRecord::Base
+  attr_accessible :name, :parent_id, :position, :type, :components_attributes
+  # Behaviours
+  has_ancestry :cache_depth => true
+  default_scope :order => 'position'
+  has_many :components, :as => :attachable, :dependent => :destroy
+  accepts_nested_attributes_for :components, :allow_destroy => true
 end
