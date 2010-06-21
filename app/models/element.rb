ba0913579b5ed163400c4d9cbe536b76788ef831
Keith Salisbury <keithsalisbury@gmail.com>
2010-06-21 01:00:47 +0100

    changed element.component to has_one relationship

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,8 +1,8 @@
 class Element < ActiveRecord::Base
-  attr_accessible :name, :parent_id, :position, :type, :components_attributes
+  attr_accessible :name, :parent_id, :position, :type, :component_attributes
   # Behaviours
   has_ancestry :cache_depth => true
   default_scope :order => 'position'
-  has_many :components, :as => :attachable, :dependent => :destroy
+  has_one :component, :as => :attachable, :dependent => :destroy
   accepts_nested_attributes_for :components, :allow_destroy => true
 end
