675a3b65570538ff1dc4a0675ba8233201abd4b1
Keith Salisbury <keithsalisbury@gmail.com>
2010-06-21 01:17:19 +0100

    added options_for_select helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def options_for_select(class_or_item, mover = nil)
+    class_or_item = class_or_item.roots if class_or_item.is_a?(Class)
+    items = Array(class_or_item)
+    result = [["",""]]
+    items.each do |root|
+      result += root.subtree.collect do |i|
+        #if mover.nil? || mover.new_record? || mover.move_possible?(i)
+          [yield(i), i.id] #unless i == mover
+        #end
+      end.compact
+    end
+    result
+  end
 end
