0292e4a0e4b695cb929d8462331bce19df7693f9
Keith Salisbury <keithsalisbury@gmail.com>
2010-06-28 13:35:33 +0100

    implemented the set_layout method for page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,12 @@ class PagesController < InheritedResources::Base
     fresh_when(:etag => @page, :last_modified => @page.updated_at.utc, :public => true)
     show!
   end
+  private
+    def set_layout
+      (action_name == "show"  && @page && @page.layout_name) || 'page'
+    end
+  protected
+    def collection
+      # @pages ||= end_of_association_chain.paginate(:page => params[:page])
+    end
 end
