90532a9f31af7c7418602caeaee55e04b4e5c1e0
Keith Salisbury <keithsalisbury@gmail.com>
2010-07-07 20:59:28 +0100

    added domains edit views

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,4 +1,9 @@
 class DomainsController < InheritedResources::Base
   actions :all, :except => [ :new, :destroy ]
   filter_resource_access
+  def edit
+    edit! do |format|
+      format.js { render :layout => false  }
+    end
+  end
 end
